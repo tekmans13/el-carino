38f148b7a775da1512a064114c3f02c857ac5487
Tekmans <tekmans@no-log.org>
2026-09-23 22:44:42 +0200

    test(e2e): update homepage title

--- tests/e2e/home.spec.js
+++ tests/e2e/home.spec.js
@@ -2,5 +2,5 @@ import { test, expect } from '@playwright/test';
 
 test('homepage loads', async ({ page }) => {
   await page.goto('/');
-  await expect(page.locator('body')).toContainText('El Carino');
+  await expect(page.locator('body')).toContainText('El Cariño');
 });
